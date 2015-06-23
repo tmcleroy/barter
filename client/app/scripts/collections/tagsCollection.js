a781be22d6e34f9a3f1fd4a7817aebab53f31e8a
Tommy <tmcleroy@gmail.com>
2015-06-22 23:08:47 -0500

    disallow dupes for tags in create request view

--- client/app/scripts/collections/tagsCollection.js
+++ client/app/scripts/collections/tagsCollection.js
@@ -2,7 +2,11 @@ var Tag = require('../models/tagModel');
 
 var TagsCollection = Backbone.Collection.extend({
   model: Tag,
-  url: '/api/tags'
+  url: '/api/tags',
+  add: function (tag) {
+    var isDupe = this.any(t => t.get('name') === tag.get('name'));
+    return isDupe ? false : Backbone.Collection.prototype.add.call(this, tag);
+  }
 });
 
 module.exports = TagsCollection;
