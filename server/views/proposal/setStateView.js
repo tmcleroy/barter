5c515a92499d54289ea8f3df1a8d8e611dbc2c59
Tommy <tmcleroy@gmail.com>
2015-07-05 15:53:03 -0500

    reject other proposals when one is accepted, refactor proposal render logic

--- server/views/proposal/setStateView.js
+++ server/views/proposal/setStateView.js
@@ -1,12 +1,27 @@
+var _ = require('lodash');
 var models = require('../../models');
+var Sequelize = require('sequelize');
 
 var handler = function (req, res) {
   models.Proposal.findById(req.params.id).then(function (proposal) {
     // ensure the proposal's request belongs to the current user
     proposal.getRequest().then(function (request) {
-      if (request.UserId == req.user.id) {
+      if (request.UserId === req.user.id) {
         proposal.updateAttributes({ state: req.body.state }).then(function (proposal) {
-          res.status(200).send(proposal);
+          // reject all the other proposals if this one is being accepted
+          if (parseInt(req.body.state, 10) === 1) {
+            request.getProposals({ where: { state: { ne: 1 } } }).then(function (proposals) {
+              var promises = [];
+              _.each(proposals, function (proposal) {
+                promises.push(proposal.updateAttributes({ state: -1 }));
+              });
+              Sequelize.Promise.all(promises).then(function () {
+                res.status(200).send(proposal);
+              });
+            });
+          } else {
+            res.status(200).send(proposal);
+          }
         });
       } else {
         res.status(401).send('you do not have the proper permissions to alter the state of this request');
--- server/views/submission/createView.js
+++ server/views/submission/createView.js
@@ -4,14 +4,15 @@ var Sequelize = require('sequelize');
 var handler = function (req, res) {
   // assure that the accepted proposal belongs to the current user
   models.Request.findById(req.body.requestId).then(function (request) {
-    request.getProposals({ where: { state: 1 } }).then(function (proposal) {
-      proposal = proposal[0];
+    request.getProposals({ where: { state: 1 } }).then(function (proposals) {
+      var proposal = proposals[0];
       if (proposal.UserId === req.user.id) {
         models.Submission.create({
           body: req.body.body,
           link: req.body.link
         }).then(function (submission) {
           var promises = [];
+          // FIXME there may be a way to bulk set these attrs
           promises.push(submission.setUser(req.user));
           promises.push(submission.setRequest(request));
           promises.push(submission.setProposal(proposal));
