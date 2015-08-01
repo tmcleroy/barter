5ca3c6e5bfce8dab737d2f0ca6b5213036322e6e
Tommy <tmcleroy@gmail.com>
2015-07-31 20:22:00 -0500

    gulp task to deploy front-end to s3

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,10 @@
+var _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
+var argv = require('yargs').argv;
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+$.parallelize = require('concurrent-transform');
 var browserify = require('browserify');
 var babelify = require("babelify");
 var source = require('vinyl-source-stream');
@@ -8,6 +14,51 @@ var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
 var underscorify = require('node-underscorify');
 var sass = require('gulp-sass');
+var awspublish = require('gulp-awspublish');
+
+var S3_CONFIG = {
+  profile: 'barter',
+  region: 'us-west-2',
+  bucket: 'barter.app'
+};
+
+
+gulp.task('watch', function () {
+  gulp.start('javascript');
+  gulp.start('sass');
+  gulp.watch(['client/app/**/*.js', 'client/app/**/*.ejs'], ['javascript']);
+  gulp.watch(['client/app/**/*.scss'], ['sass']);
+});
+
+gulp.task('deploy', function () {
+  var awsCredentials = JSON.parse(fs.readFileSync(process.env.HOME + '/.aws/credentials.json')).barter;
+
+  var publisher = awspublish.create({
+    accessKeyId: awsCredentials.aws_access_key_id,
+    secretAccessKey: awsCredentials.aws_secret_access_key,
+    region: S3_CONFIG.region,
+    params: {
+      Bucket: S3_CONFIG.bucket
+    }
+  });
+
+  var target = argv.target;
+
+  if (!target) {
+    throw new gutil.PluginError('publish', 'Specify a target using --target=<name>');
+  }
+
+  var destPath = 'public/dist/' + target + '/';
+  var headers = {
+    'Cache-Control': 'max-age=60, must-revalidate'
+  };
+  return gulp.src('server/public/dist/**/*')
+    .pipe($.rename(function (p) {
+      p.dirname = path.join(destPath, p.dirname);
+    }))
+    .pipe($.parallelize(publisher.publish(headers), 5))
+    .pipe(awspublish.reporter());
+});
 
 gulp.task('javascript', function () {
 
@@ -41,11 +92,4 @@ gulp.task('sass', function () {
       ]
     }).on('error', sass.logError))
       .pipe(gulp.dest('./server/public/dist/styles'));
-});
-
-gulp.task('watch', function () {
-  gulp.start('javascript');
-  gulp.start('sass');
-  gulp.watch(['client/app/**/*.js', 'client/app/**/*.ejs'], ['javascript']);
-  gulp.watch(['client/app/**/*.scss'], ['sass']);
 });
