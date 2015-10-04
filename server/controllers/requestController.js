d86c6e05487973997391c022875852525d9fcd44
Tommy <tmcleroy@gmail.com>
2015-10-04 18:01:58 -0500

    fix tag subscription notifications bug by including lodash

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -3,17 +3,12 @@ var models = require('../models');
 var Sortable = require('../helpers/sortable');
 
 var controller = {
-
   show: function (req, res) {
     models.Request.findOne({
       where: { id: req.params.id},
       include: [
-        {
-          model: models.User
-        },
-        {
-          model: models.Tag
-        },
+        { model: models.User },
+        { model: models.Tag },
         {
           model: models.Comment,
           include: [models.User]
@@ -22,9 +17,7 @@ var controller = {
           model: models.Proposal,
           include: [models.User, models.Submission]
         },
-        {
-          model: models.Submission
-        }
+        { model: models.Submission }
       ]
     }).then(function (request) {
       res.status(200).send(request);
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 var models = require('../models');
 
 var controller = {
